cb822755f31ea92f340701ae29fb915548f1279b
William Davis <davis.wd@rhodesstate.edu>
2018-02-09 21:15:28 -0500

    Setup models for database

---
+++ Kupanga/Models/EmployeeAccessViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Kupanga.Models.Repository;
+
+namespace Kupanga.Models
+{
+    public class EmployeeAccessViewModel
+    {
+        public List<SubmittedQuote> SubmittedQuotes { get; set; }
+    }
+}
---
+++ Kupanga/Scripts/AjaxScripts.js
@@ -0,0 +1,11 @@
+﻿function viewDetails(id) {
+    $.ajax({
+        url: "/EmployeeAccess/QuoteDetails",
+        cache: false,
+        type: "POST",
+        data: { id: id },
+        success: function (html) {
+            $("#divModalBody").html(result);
+        }
+    });
+}
